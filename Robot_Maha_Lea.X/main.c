8ced287e9c2e036c6f13510f4d078ea07020cdb1
TABLE3 <touloniut@gmail.com>
2024-10-22 18:57:12 +0200

    ajout des 32cas

--- Robot_Maha_Lea.X/main.c
+++ Robot_Maha_Lea.X/main.c
@@ -195,85 +195,201 @@ void SetNextRobotStateInAutomaticMode() {
         sensorState |= 1 << 1;
     if (robotState.distanceTelemetreDroit2 < 30)
         sensorState |= 1 << 0;
-    
+
     switch (sensorState) {
+
         case 0b00000:
+
             nextStateRobot = STATE_AVANCE;
+
             break;
+
         case 0b00001:
-            nextStateRobot = STATE_AVANCE;
+
+            nextStateRobot = STATE_TOURNE_DROITE;
+
             break;
+
         case 0b00010:
-            nextStateRobot = STATE_AVANCE;
+
+            nextStateRobot = STATE_TOURNE_DROITE;
+
             break;
+
         case 0b00011:
-            nextStateRobot = STATE_AVANCE;
+
+            nextStateRobot = STATE_TOURNE_DROITE;
+
             break;
+
+        case 0b00100:
+
+            nextStateRobot = STATE_TOURNE_SUR_PLACE_GAUCHE;
+
+            break;
+
         case 0b00101:
-            nextStateRobot = STATE_AVANCE;
+
+            nextStateRobot = STATE_TOURNE_DROITE;
+
             break;
+
         case 0b00110:
-            nextStateRobot = STATE_AVANCE;
+
+            nextStateRobot = STATE_TOURNE_DROITE;
+
             break;
+
         case 0b00111:
-            nextStateRobot = STATE_AVANCE;
+
+            nextStateRobot = STATE_TOURNE_DROITE;
+
             break;
+
         case 0b01000:
-            nextStateRobot = STATE_AVANCE;
+
+            nextStateRobot = STATE_TOURNE_GAUCHE;
+
             break;
+
         case 0b01001:
-            nextStateRobot = STATE_AVANCE;
+
+            nextStateRobot = STATE_TOURNE_SUR_PLACE_DROITE;
+
             break;
+
         case 0b01010:
-            nextStateRobot = STATE_AVANCE;
+
+            nextStateRobot = STATE_RECULE;
+
             break;
+
         case 0b01011:
-            nextStateRobot = STATE_AVANCE;
+
+            nextStateRobot = STATE_TOURNE_SUR_PLACE_DROITE;
+
             break;
+
         case 0b01100:
-            nextStateRobot = STATE_AVANCE;
+
+            nextStateRobot = STATE_TOURNE_GAUCHE;
+
             break;
-        case 0b00000:
-            nextStateRobot = STATE_AVANCE;
-            break;
-        case 0b00001:
-            nextStateRobot = STATE_AVANCE;
-            break;
-        case 0b00010:
-            nextStateRobot = STATE_AVANCE;
-            break;
-        case 0b00011:
-            nextStateRobot = STATE_AVANCE;
-            break;
-        case 0b00101:
-            nextStateRobot = STATE_AVANCE;
-            break;
-        case 0b00110:
-            nextStateRobot = STATE_AVANCE;
-            break;
-        case 0b00111:
-            nextStateRobot = STATE_AVANCE;
-            break;
-        case 0b01000:
-            nextStateRobot = STATE_AVANCE;
-            break;
-        case 0b01001:
-            nextStateRobot = STATE_AVANCE;
-            break;
-        case 0b01010:
-            nextStateRobot = STATE_AVANCE;
-            break;
-        case 0b01011:
-            nextStateRobot = STATE_AVANCE;
-            break;
-        case 0b01100:
-            nextStateRobot = STATE_AVANCE;
-            break;
+
         case 0b01101:
-            nextStateRobot = STATE_AVANCE;
+
+            nextStateRobot = STATE_TOURNE_SUR_PLACE_DROITE;
+
             break;
-            
-            
+
+        case 0b01110:
+
+            nextStateRobot = STATE_RECULE;
+
+            break;
+
+        case 0b01111:
+
+            nextStateRobot = STATE_TOURNE_DROITE;
+
+            break;
+
+        case 0b10000:
+
+            nextStateRobot = STATE_TOURNE_GAUCHE;
+
+            break;
+
+        case 0b10001:
+
+            nextStateRobot = STATE_AVANCE;
+
+            break;
+
+        case 0b10010:
+
+            nextStateRobot = STATE_RECULE;
+
+            break;
+
+        case 0b10011:
+
+            nextStateRobot = STATE_RECULE;
+
+            break;
+
+        case 0b10100:
+
+            nextStateRobot = STATE_TOURNE_GAUCHE;
+
+            break;
+
+        case 0b10101:
+
+            nextStateRobot = STATE_ARRET;
+
+            break;
+
+        case 0b10110:
+
+            nextStateRobot = STATE_ARRET;
+
+            break;
+
+        case 0b10111:
+
+            nextStateRobot = STATE_ARRET;
+
+            break;
+
+        case 0b11000:
+
+            nextStateRobot = STATE_TOURNE_GAUCHE;
+
+            break;
+
+        case 0b11001:
+
+            nextStateRobot = STATE_ARRET;
+
+            break;
+
+        case 0b11010:
+
+            nextStateRobot = STATE_ARRET;
+
+            break;
+
+        case 0b11011:
+
+            nextStateRobot = STATE_ARRET;
+
+            break;
+
+        case 0b11100:
+
+            nextStateRobot = STATE_TOURNE_GAUCHE;
+
+            break;
+
+        case 0b11101:
+
+            nextStateRobot = STATE_ARRET;
+
+            break;
+
+        case 0b11110:
+
+            nextStateRobot = STATE_TOURNE_GAUCHE;
+
+            break;
+
+        case 0b11111:
+
+            nextStateRobot = STATE_RECULE;
+
+            break;
+
     }
     
     
